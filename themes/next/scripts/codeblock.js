61eca0977dae37c23b111990132f4eeeb30ba2e1
mutoe <mutoe@foxmail.com>
2019-03-25 16:12:53 +0800

    update: profile

--- themes/next/scripts/codeblock.js
+++ themes/next/scripts/codeblock.js
@@ -1,21 +1,16 @@
-var attributes = [
-  'autocomplete="off"',
-  'autocorrect="off"',
-  'autocapitalize="off"',
-  'spellcheck="false"',
-  'contenteditable="true"'
-]
-
-var attributesStr = [] || attributes.join(' ')
+var languageMap = {
+  js: 'javascript',
+}
 
 hexo.extend.filter.register('after_post_render', function (data) {
   const regex = /<figure class="highlight ([a-zA-Z]+)">.*?<\/figure>/
   while (regex.test(data.content)) {
     data.content = data.content.replace(regex, function () {
       var language = RegExp.$1 || 'plain'
+      if (languageMap[language]) language = languageMap[language]
       var lastMatch = RegExp.lastMatch
       lastMatch = lastMatch.replace(/<figure class="highlight /, '<figure class="iseeu highlight /')
-      return `<div class="highlight-wrap" ${attributesStr} data-rel="${language}">${lastMatch}</div>`
+      return `<div class="highlight-wrap" data-rel="${language.toUpperCase()}">${lastMatch}</div>`
     })
   }
   return data
