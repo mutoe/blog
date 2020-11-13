7ba6a0a7161c273c3b7beeee3268a2a1a8e96ed7
mutoe <mutoe@foxmail.com>
2020-11-14 00:12:40 +0800

    update content

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,43 @@
-var gulp = require('gulp')
-var htmlmin = require('gulp-htmlmin')
-var exec = require('child_process').exec
+const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
+const exec = require('child_process').exec
 
-var root = './public'
-var buildDir = root
-var datas = {
-  html: [root + '/**/*.html'],
-  js: [root + '/**/*.js', '!' + root + '/**/*.min.js'],
-  css: [root + '/**/*.css'],
+const root = './public'
+const buildDir = root
+const files = {
+  html: [ `${root}/**/*.html` ],
+  js: [ `${root}/**/*.js`, `!${root}/**/*.min.js` ],
+  css: [ `${root}/**/*.css` ],
 }
 
-const cleanTask = function(cb) {
-  exec('hexo clean', function(err) {
+const cleanTask = function (cb) {
+  exec('hexo clean', function (err) {
     if (err) return cb(err)
     cb()
   })
 }
 
-const buildTask = function(cb) {
-  exec('hexo g', function(err) {
+const buildTask = function (cb) {
+  exec('hexo g', function (err) {
     if (err) return cb(err)
     cb()
   })
 }
 
-const htmlminTask = function() {
+const htmlminTask = function () {
   return gulp
-    .src(datas.html)
-    .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
+    .src(files.html)
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true,
+      minifyCSS: true,
+      minifyJS: true,
+    }))
     .pipe(gulp.dest(buildDir))
 }
 
-const releaseTask = function(cb) {
-  exec('hexo d', function(err) {
+const releaseTask = function (cb) {
+  exec('hexo d', function (err) {
     if (err) return cb(err)
     cb()
   })
