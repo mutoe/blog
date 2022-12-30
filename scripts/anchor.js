33661ce948fcae50f0d628806cac3aa081ea4894
mutoe <imutoe@gmail.com>
2022-12-31 04:11:45 +0800

    fix style

--- scripts/anchor.js
+++ scripts/anchor.js
@@ -1,5 +1,7 @@
 /* global hexo */
 
 hexo.extend.filter.register('after_post_render', data => {
+    // before <h1 id="foo"><a class="headerlink"></a>title</h1>
+    // after  <h1 id="foo"><a class="headerlink">title</a></h1>
     data.content = data.content.replace(/(<(h[1-6]) id=.*?>)(<\/a>)(.*?)(<\/\2>)/g, '$1$4$3$5')
 });
